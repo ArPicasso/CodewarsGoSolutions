3fa95ca5083b4eebd2c68255c01d2796a9176a42
Picasso <ar.picassopablo@gmail.com>
2025-08-01 23:25:51 +0300

    Task BooleanToString

---
+++ go/practise.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func main(){
+	fmt.Println(BoolToWord(true))
+}
+
+func BoolToWord(word bool) string {
+  if word == true{
+    return "Yes"
+  } else{
+    return "No"
+  }
+}
