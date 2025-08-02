55155eecf4c8cf02cefad561c240dfddf7eebf89
Picasso <ar.picassopablo@gmail.com>
2025-08-02 12:46:12 +0300

    Olimpiad Tasks and Codewars tasks updated

--- go/practise.go
+++ go/practise.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
-
-func main(){
-	fmt.Println(BoolToWord(true))
-}
-
-func BoolToWord(word bool) string {
-  if word == true{
-    return "Yes"
-  } else{
-    return "No"
-  }
+func main() {
+	for i:=0; i<100; i++{
+		n_re := fmt.Sprintf("%b",i)
+		if i % 2 == 0{
+			n_re = "10" + n_re 
+		} else {
+			n_re = "1" + n_re + "01"
+		}
+		R, _ := strconv.ParseInt(n_re,2,64)
+		
+		if i == 4{
+			fmt.Println("Число:", i, "n_re:", n_re, "R:", R)
+		}
+		if R < 30{
+			fmt.Println(i)
+		}
+	}
+	
 }
