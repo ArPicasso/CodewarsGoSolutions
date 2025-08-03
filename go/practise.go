629c84ef95b5dda4880cdf17983b61a1dd059ec7
Picasso <ar.picassopablo@gmail.com>
2025-08-03 23:04:36 +0300

    8 kyu tasks

--- go/practise.go
+++ go/practise.go
@@ -2,25 +2,16 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
-	for i:=0; i<100; i++{
-		n_re := fmt.Sprintf("%b",i)
-		if i % 2 == 0{
-			n_re = "10" + n_re 
-		} else {
-			n_re = "1" + n_re + "01"
-		}
-		R, _ := strconv.ParseInt(n_re,2,64)
-		
-		if i == 4{
-			fmt.Println("Число:", i, "n_re:", n_re, "R:", R)
-		}
-		if R < 30{
-			fmt.Println(i)
-		}
-	}
-	
+	secretInfo := "******"
+	recievedSignal := &secretInfo
+
+	fmt.Println(recievedSignal)
+	fmt.Println("Recieved password:",workWithSignal(recievedSignal))
+}
+
+func workWithSignal(signal *string) string{
+	return *signal
 }
